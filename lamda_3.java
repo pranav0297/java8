37b5292dd064319c9e5996763b4cfeb93e8d1306
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-10-04 17:12:36 +0530

    first commit

--- lamda_3.java
+++ lamda_3.java
@@ -1,2 +1,19 @@
-public class lamda_3 {
+interface MyInterface{
+    void display(int a , int b);
+}
+public class test {
+
+    public void add(int a ,int b){
+        System.out.println(a+b);
+    }
+    public void mul(int a ,int b){
+        System.out.println(a*b);
+    }
+    public static void main(String[] args) {
+        test obj = new test();
+        MyInterface ref = obj::add;
+        MyInterface ref1 = obj::mul;
+        ref.display(2,5);
+        ref1.display(2,5);
+    }
 }
